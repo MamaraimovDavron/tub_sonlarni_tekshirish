96f6da005991e4233a02b676f3a13f979786bf46
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2022-07-08 08:38:56 +0500

    added style.css

---
+++ app.js
@@ -0,0 +1,16 @@
+function tub_son(n){
+    if(n % 2 != 0){
+        if((n / n == 1) && (n / 1 == n)){
+            console.log(true);
+        }
+    }
+    if(n == 1){
+        console.log("Tub ham emas, murakkab son ham emas");
+    }
+
+    else{
+        console.log(false);
+    }
+}
+
+tub_son(1);
